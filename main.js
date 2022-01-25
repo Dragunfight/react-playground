147e7cc020a34c933c25eca1cc4c426427785a77
Dragunfight <dragunfight@gmail.com>
2022-01-25 15:36:24 +0100

    first

--- main.js
+++ main.js
@@ -1,37 +1,90 @@
-function App(props) {
-    React.useEffect(() => {
-        fetch('https://jsonplaceholder.typicode.com/users')
-        .then(response => response.json())
-        .then(usersObject => setUsers(usersObject))
-    }, [])
+// function App(props) {
+//     React.useEffect(() => {
+//         fetch('https://jsonplaceholder.typicode.com/users')
+//         .then(response => response.json())
+//         .then(usersObject => setUsers(usersObject))
+//     }, [])
 
-    const [users, setUsers] = React.useState([]);
+//     const [users, setUsers] = React.useState([]);
+
+
+
+//     return(
+//         <React.Fragment>
+//         {
+//             users.map((u) =>(
+//                 <UserCard key={u.id} user={u}/>
+//             ))
+//         }
+//         </React.Fragment>
+//     )
+// }
+
+// function UserCard({user}) {
+    
+
+//     return (
+//         <ul className="user-card">
+//             <li>{user.name}</li>
+//             <li>{user.email}</li>
+//             <li>{user.company.name}</li>
+//             <li>{user.phone}</li>
+//             <li>{user.website}</li>
+//         </ul>
+//     )
+// }
+
+// ReactDOM.render(<App/>, document.querySelector('#app'))
+
+
+function NameForm() {
+    const [name, setName] = React.useState('');
+
+    const handleChange = (e) => {
+        e.preventDefault();
+        setName(e.target.value);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(`Le nom a été soumis: ${name}`);
+    }
 
 
 
     return(
-        <React.Fragment>
-        {
-            users.map((u) =>(
-                <UserCard key={u.id} user={u}/>
-            ))
-        }
-        </React.Fragment>
-    )
+    <form onSubmit={handleSubmit}>
+        <label>
+        Nom :
+        <input type="text" value={name} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Envoyer" />
+    </form>
+);
 }
 
-function UserCard({user}) {
+
+function EssayForm() {
     
-
+  
+    const handleChange = (e) => {
+        e.preventDefault();
+        setState(e.target.value);
+    }
+  
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Un essai a été envoyé : ' );
+    }
+  
+    
     return (
-        <ul className="user-card">
-            <li>{user.name}</li>
-            <li>{user.email}</li>
-            <li>{user.company.name}</li>
-            <li>{user.phone}</li>
-            <li>{user.website}</li>
-        </ul>
-    )
-}
-
-ReactDOM.render(<App/>, document.querySelector('#app'))
+      <form onSubmit={handleSubmit}>
+        <label>
+          Essay:
+          <textarea value={this.state.value} onChange={handleChange} />
+        </label>
+        <input type="submit" value="Envoyer" />
+      </form>
+    );
+  }
